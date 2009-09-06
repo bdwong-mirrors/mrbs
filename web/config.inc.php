f470ba3cfc018ff1d23738d635d3f902b4436c78
proj_admin <proj_admin@25f7e783-145e-4f2d-a9f8-dd7182bfe9c7>
2009-09-06 17:01:46 +0000

    Reversed accidental changes to config.inc.php (back to 1176)

--- web/config.inc.php
+++ web/config.inc.php
@@ -24,16 +24,16 @@
  ******************/
 // Which database system: "pgsql"=PostgreSQL, "mysql"=MySQL,
 // "mysqli"=MySQL via the mysqli PHP extension
-$dbsys = "mysqli";
+$dbsys = "mysql";
 // Hostname of database server. For pgsql, can use "" instead of localhost
 // to use Unix Domain Sockets instead of TCP/IP.
 $db_host = "localhost";
 // Database name:
-$db_database = "mrbs-dev";
+$db_database = "mrbs";
 // Database login user name:
 $db_login = "mrbs";
 // Database login password:
-$db_password = 'mrbs';
+$db_password = 'mrbs-password';
 // Prefix for table names.  This will allow multiple installations where only
 // one database is available
 $db_tbl_prefix = "mrbs_";
@@ -44,8 +44,8 @@ $db_tbl_prefix = "mrbs_";
 /* Add lines from systemdefaults.inc.php here to change the default
    configuration. Do _NOT_ modify systemdefaults.inc.php. */
    
-$auth["type"] = "db";
-$auth["admin"][] = "paul";
+   
+
    
    
 // This next section must come at the end of the config file - ie after any
